17afbb373fca66f4304e6d44d9a0288abe577bbf
Zannan <zannan.work@gmail.com>
2024-08-01 00:08:23 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/task_chess_board.dart';
-import 'package:flutter_widgets/trips_mountain2.dart';
-import 'package:flutter_widgets/ui_color_task.dart';
-import 'package:flutter_widgets/ui_login.dart';
-import 'package:flutter_widgets/ui_signup.dart';
-import 'package:flutter_widgets/ui_task.dart';
+import 'package:flutter_widgets/flutter_basics/task_chess_board.dart';
+import 'package:flutter_widgets/ui_task_1/trips_mountain2.dart';
+import 'package:flutter_widgets/flutter_basics/ui_color_task.dart';
+import 'package:flutter_widgets/flutter_basics/ui_login.dart';
+import 'package:flutter_widgets/flutter_basics/ui_signup.dart';
+import 'package:flutter_widgets/flutter_basics/ui_task.dart';
 import 'package:flutter_widgets/ui_task_1/trips_moutain.dart';
-import 'package:flutter_widgets/widget_container.dart';
-import 'package:flutter_widgets/widget_icon.dart';
-import 'package:flutter_widgets/widget_image.dart';
-import 'package:flutter_widgets/widget_image_2.dart';
-import 'package:flutter_widgets/widget_image_3.dart';
-import 'package:flutter_widgets/widget_row_and_column.dart';
-import 'package:flutter_widgets/widget_text.dart';
-import 'package:flutter_widgets/widget_textbutton.dart';
+import 'package:flutter_widgets/flutter_basics/widget_container.dart';
+import 'package:flutter_widgets/flutter_basics/widget_icon.dart';
+import 'package:flutter_widgets/flutter_basics/widget_image.dart';
+import 'package:flutter_widgets/flutter_basics/widget_image_2.dart';
+import 'package:flutter_widgets/flutter_basics/widget_image_3.dart';
+import 'package:flutter_widgets/flutter_basics/widget_row_and_column.dart';
+import 'package:flutter_widgets/flutter_basics/widget_text.dart';
+import 'package:flutter_widgets/flutter_basics/widget_textbutton.dart';
 
 void main() {
   runApp(const MyApp());
